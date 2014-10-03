8101481ec0378003d615c8389aecb7ed91db7bc2
Mehran Hadidi <mehran.hadidi@gmail.com>
2014-10-03 17:47:19 +0330

    index file that init the project

--- application/Bootstrap.php
+++ application/Bootstrap.php
@@ -1,2 +1,22 @@
-<?php
-echo "BOOOOOT";
+<?php defined('COREPATH') or die('No direct script access.');
+
+/**
+ * Set the default time zone.
+ *
+ * @link http://www.php.net/manual/timezones
+ */
+date_default_timezone_set('Asia/Tehran');
+
+/**
+ * Set the default locale.
+ *
+ * @link http://www.php.net/manual/function.setlocale
+ */
+setlocale(LC_ALL, 'en_US.utf-8');
+
+// pars the address bar params to readable addresses like controller, method, param, etc.
+$base_url = isset($_GET['url']) ? $_GET['url'] : null;
+$base_url = rtrim($base_url, '/');
+$base_url = explode('/', $base_url);
+
+print_r($base_url);
